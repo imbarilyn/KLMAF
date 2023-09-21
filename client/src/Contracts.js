4568484803aaed7b81d07cb1e1812e25ed732cf6
imbarilyn <linahmuhonjaimbari@gmail.com>
2023-09-21 06:23:46 +0300

    Add boostrap package

--- client/src/Contracts.js
+++ client/src/Contracts.js
@@ -3,8 +3,18 @@ import React from 'react'
 
 function Contracts() {
   return (
-    <div>
+    <div className='main-content'>
+      <div className='search-tab'>
+        <h6>search <input type='search' style={{border:"1px solid black", height:"30px"}}/></h6>
+      </div>
+      <div className='add-contract'>
+        <button type="button" class="btn btn-success">Add+</button>
+      </div>
+      <div className='Contracts-display'>
         
+      </div>
+
+      
     </div>
   )
 }
