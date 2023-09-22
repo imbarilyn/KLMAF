108815ca4379d295d8a0d627274e1855d7c13924
imbarilyn <linahmuhonjaimbari@gmail.com>
2023-09-22 06:41:52 +0300

    Add table for contacts

--- client/src/Contracts.js
+++ client/src/Contracts.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React,  { useState, useEffect } from 'react';
+
 
 
 function Contracts() {
+  const [columns, setColumns] = useState ([]);
+  const [records, setRecords] = useState([]);
+
+
+
+  useEffect(()=>{
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(res =>{
+      setColumns(Object.keys(res.products[0]))
+      // console.log(res.products)
+      setRecords(res.products)
+    })
+  }, [])
   return (
     <div className='main-content'>
       <div className='search-tab'>
@@ -11,10 +26,27 @@ function Contracts() {
         <button type="button" class="btn btn-success">Add+</button>
       </div>
       <div className='Contracts-display'>
-        
-      </div>
-
-      
+            <table class="table table-striped table-hover mt-5">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Country</th>
+                  <th scope="col">Expiry date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  {
+                    
+                  }
+                  <th scope="row"></th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>   
+              </tbody>
+            </table>
+      </div>     
     </div>
   )
 }
