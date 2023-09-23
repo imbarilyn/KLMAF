0460e4985e0a608b11c34046c324e926dc151b9c
imbarilyn <linahmuhonjaimbari@gmail.com>
2023-09-23 09:54:58 +0300

    Add contract creating from

--- client/src/Create.js
+++ client/src/Create.js
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function Create() {
   return (
-    <div>Create</div>
+    <div className = "create-contract-form">
+      <form>
+          <div class="form-group">
+            <label for="exampleInputName">Contract Name</label>
+            <input type="text" class="form-control" id="exampleInputName" placeholder="Enter contract name" />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputCountry">Country</label>
+            <input type="text" class="form-control" id="exampleInputCountry" placeholder="Enter country"/>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEpiryDate">Expiry Date</label>
+            <input type="date" class="form-control" id="exampleInputExpiryDate" placeholder="Enter expiry date"/>
+          </div>
+         
+          <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
+    </div>
   )
 }
