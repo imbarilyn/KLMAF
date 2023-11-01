5fc8de99e8ea1a7e1901b8957689e6b9f394b321
imbarilyn <linahmuhonjaimbari@gmail.com>
2023-11-01 04:40:19 +0300

    add a form on create file

--- client/src/Create.js
+++ client/src/Create.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './Create.css';
+// import travel from './photos/createbg.png'
 
-export default function Create() {
+export default function Create(e) {
+  // e.preventDefault();
+  const submitHandler = (e)=>{
+    
+  }
+
   return (
     <div className = "create-contract-form">
-      <form>
+      <form onSubmit = {submitHandler()}>
           <div class="form-group">
             <label for="exampleInputName">Contract Name</label>
             <input type="text" class="form-control" id="exampleInputName" placeholder="Enter contract name" />
