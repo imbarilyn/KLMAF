f4f4a9384a4c9dc3937fc1882494732e1462d72c
imbarilyn <linahmuhonjaimbari@gmail.com>
2023-11-03 04:57:38 +0300

    add seeds to my table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Contract.create!([
+    {name: "Panari", expirydate: "20 Sept 2009", country: "Kenya"},
+    {name: "Lazizi", expirydate: "20 Sept 2027", country: "Kenya"},
+    {name: "Auamist", expirydate: "20 Sept 2030", country: "Uganda"},
+    {name: "Hyatt", expirydate: "20 Sept 2026", country: "Tanzania"}
+    ])
