267103236f6e7c95c5a5a24c82d191a7d23e5f30
imbarilyn <linahmuhonjaimbari@gmail.com>
2023-11-03 06:05:47 +0300

    add routes for all conttracts and identical contract

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- get "/contract", to: "contract#index"
- 
+# gets all the contracts in our db
+ get "/contracts", to: "contract#index"
+
+#  gets contract by id
+
+get "/contracts/:id", to: "contract#show"
+
 end
