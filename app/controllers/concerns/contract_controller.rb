806a83c661c27bb319c223e1d9072fe6bcca41fb
imbarilyn <linahmuhonjaimbari@gmail.com>
2023-11-03 06:04:58 +0300

    add controller actions

--- app/controllers/concerns/contract_controller.rb
+++ app/controllers/concerns/contract_controller.rb
@@ -1,5 +1,19 @@
 class ContractController < ApplicationController
-    def summary
-        "#{self.name}: $#{self.country}"
+   
+    def index
+        contracts = Contract.all
+        render json: contracts, except: [:created_at, :updated_at], methods: [:summary]
     end
+
+    def show
+        contract = Contract.find_by(id: params[:id])
+        if contract
+            render json: contract, except: [:created_at, :updated_at], methods: [:summary]
+        else
+            render json: {error: "Contract not found"}, status: 404
+        end
+    end
+
+
+
 end
