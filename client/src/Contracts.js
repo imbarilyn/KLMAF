1fd99327bd148890fc93e40b06366eb63c21745c
imbarilyn <linahmuhonjaimbari@gmail.com>
2023-11-12 16:30:58 +0300

    add props

--- client/src/Contracts.js
+++ client/src/Contracts.js
@@ -1,36 +1,63 @@
 import React from 'react';
 import './Contracts.css'
+import { Link } from 'react-router-dom';
 // import { RiSearchLine } from 'react-icons/ri';
 
 
-function Contracts({contract}) {
+function Contracts({passContracts}) {
   // const [columns, setColumns] = useState ([]);
   // const [id, name, expirydate, country] =  passContracts 
   
-  const {id, name, expirydate, country} = contract
+  // const {id, name, expirydate, country} = passContracts
   // console.log({name})
-  
-  // console.log(passContracts)
+  passContracts.map((record) =>{
 
+  console.log(record)
+
+
+  })
   return (
     <div className='main-content'>
+        <div className='contracts-top'style={{float: "right"}}>
+          <div className='search-tab'style={{ marginRight: "10px", marginTop: "4px"}}>
+            <label>search  </label><input type='search' style={{border:"1px solid black", height:"30px"}}/>
+          </div>
+          <br></br>
+          <div className='add-contract'>
+            <Link to = "/create"><button type="button" className="btn btn-success" style={{float: "right", marginRight: "3px", marginTop: "0px"}}>Add+</button></Link>
+          </div>
+      </div>
+        
       <div className='Contracts-display'>
-            <table className="table table-striped table-hover mt-5">
+            <table className="table table-hover">
               <thead>
                 <tr>                
                   <th>No.</th>
                   <th>Contract Name</th>   
                   <th>Expiry Date</th>
-                  <th>Country</th>                          
+                  <th>Country</th>  
+                  <th>Action</th>                        
                 </tr>
               </thead>
               <tbody> 
-                <tr>             
-                  <td>{id}</td>
-                  <td>{name}</td>
-                  <td>{expirydate}</td>
-                  <td>{country}</td> 
-                </tr>
+                {
+                  passContracts.map((i, j) =>(
+                    <tr>             
+                    <td>{i.id}</td>
+                    <td>{i.name}</td>
+                    <td>{i.expirydate}</td>
+                    <td>{i.country}</td> 
+                    <td>
+                      <button  type="button" class="btn btn-outline-primary" style={{margin: "4px"}}>Update</button>
+                      <button type="button" class="btn btn-outline-danger">Delete</button>
+                    </td>
+                  </tr>
+
+                  )
+                  )
+                 
+                }
+             
                 
               </tbody>
             </table>
